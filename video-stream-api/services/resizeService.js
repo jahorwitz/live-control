128a160339d99ccd69367493bb7fb032184fa3aa
Joshua Horwitz <jhorwitz@pop-os.localdomain>
2020-08-01 23:59:43 -0400

    converting sizes works correctly

--- video-stream-api/services/resizeService.js
+++ video-stream-api/services/resizeService.js
@@ -35,11 +35,21 @@ const resizeVideo = async (path, type) => {
 
 
     console.log("Height is ", height);
-    const sizesToGenerate = supportedSizes.filter(size => size.height <= height);
+    const sizesToGenerate = supportedSizes.filter(size => size.height < height);
     console.log("Generating sizes: ", JSON.stringify(sizesToGenerate, 0, 2));
 
     const outputFilePromises = [];
-    const outputStreams = [];
+    const outputFiles = [];
+
+    // If the height matches one of the supported resolutions, don't resize the original
+    const originalSize = supportedSizes.find(size => size.height === height);
+    if(originalSize) {
+        outputFiles.push({
+            path,
+            resolution: originalSize.name
+        });
+    }
+
     try {
         sizesToGenerate.forEach(outputSize => {
             const outputPath = `${path}-${outputSize.name}`;
@@ -48,16 +58,18 @@ const resizeVideo = async (path, type) => {
                 ffmpeg()
                     .input(path)
                     .format(type.ext)
+                    .videoCodec('libx264')
                     .size(`?x${outputSize.height}`)
                     .on('end', resolve)
                     .on('progress', function (progress) {
                         console.log('Processing: ' + progress.percent + '% done');
                     })
                     .on('error', console.log)
+                    // .outputOptions(['-vcodec h264', '-pix_fmt yuv444p'])
                     .output(outputPath)
                     .run();
             });
-            outputStreams.push({
+            outputFiles.push({
                 path: outputPath,
                 resolution: outputSize.name
             });
@@ -68,7 +80,7 @@ const resizeVideo = async (path, type) => {
         console.log(err);
         throw err;
     }
-    return outputStreams;
+    return outputFiles;
 };
 
 module.exports = {
